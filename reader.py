a051fe50b10842d360ee6d40c9c63eb2e38abe54
XU Kai <xukai.ken@gmail.com>
2014-10-09 10:53:59 +0800

    Dev

--- reader.py
+++ reader.py
@@ -1,10 +1,30 @@
-'''
-Created on 2014年10月8日
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
 
-@author: xukai
-'''
+__author__ = 'xukai.ken@gmail.com(Kai.XU)'
 
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+
+CLIENT_SECRETS = 'client_secrets.json'
+
+TOKEN_FILE_NAME = 'analytics.dat'
+
+MISSING_CLIENT_SECRETS_MESSAGE = '%s is missing' % CLIENT_SECRETS
+
+FLOW = flow_from_clientsecrets(CLIENT_SECRETS, scope = 'https://www.googleapis.com/auth/analytics.readonly', message = MISSING_CLIENT_SECRETS_MESSAGE)
+
+def main():
+  
+  
+  
+  pass
 
 
 if __name__ == '__main__':
-    pass
+  main()
